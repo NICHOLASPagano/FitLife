22ce192d92464af9ea789c109d719f8c2831c7c6
NICHOLASPagano <89316077+NICHOLASPagano@users.noreply.github.com>
2021-12-13 10:09:38 -0500

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,45 @@
+public class Workout()
+{
+  private String days;
+  private String workout;
+  private int reps;
+  private int sets;
+  private int numLifts;
+  
+  public Workout(String d, String w, int r, int s)
+  {
+    days = d;
+    workout = w;
+    setReps(r);
+    SetSets(s);
+  }
+  public Workout()
+  {
+    System.out.println("Please Enter the following fields: Day, Workout, Reps, and Sets");
+    /*
+    *scanner method for entry of values from GUI
+    */
+  }
+  public String toString()
+  {
+      return "Day: " + days + "\n lifts: " + workoutList();
+  }
+    //Setters
+    public setReps(int r)
+    {
+        reps = r;
+    }
+    public setSets(int s)
+    {
+        sets = s
+    }
+    //Getters
+    public int getReps()
+    {
+        return reps;
+    }
+    public int getSets()
+    {
+        return sets;
+    }
+}
